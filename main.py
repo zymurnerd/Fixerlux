b0f018fa884c610d359291d77f080eb57329efdd
Wes <wes.detweiler@gmail.com>
2022-03-29 05:37:46 -0500

    [fix] Better handle differncess between files and dirs

--- main.py
+++ main.py
@@ -6,52 +6,87 @@ import re
 from tqdm import tqdm
 
 
-def final_strip(source):
+def final_strip(source, is_dir=None):
     _source = pathlib.Path(source)
     _destination = pathlib.Path(source)
+
+    if is_dir:
+        us = _source.name
+    else:
+        us = _source.stem
 
     # Strip all dumb characters
-    _destination = _source.with_stem(_source.stem.strip(' _-@*![]#'))
-    _destination = _destination.with_stem(_destination.stem.rstrip('.'))
+    if is_dir:
+        _destination = _source.with_name(_source.name.strip(' _-@*![]#'))
+        _destination = _destination.with_name(_destination.name.rstrip('.'))
+    else:
+        _destination = _source.with_stem(_source.stem.strip(' _-@*![]#'))
+        _destination = _destination.with_stem(_destination.stem.rstrip('.'))
 
     return _destination
 
 
-def ws_to_underscore(source):
+def ws_to_underscore(source, is_dir=None):
     _source = pathlib.Path(source)
     _destination = pathlib.Path(source)
+
+    if is_dir:
+        us = _source.name
+    else:
+        us = _source.stem
 
     # Make all whitespace into underscores
-    us = _source.stem.strip()
+    us = us.strip()
     us = us.replace(" ", "_")
-    _destination = _source.with_stem(us.strip('_'))
+
+    if is_dir:
+        _destination = _source.with_name(us.strip('_'))
+    else:
+        _destination = _source.with_stem(us.strip('_'))
 
     return _destination
 
 
-def delete_nonsense(source):
+def delete_nonsense(source, is_dir=None):
     _source = pathlib.Path(source)
     _destination = pathlib.Path(source)
 
+    if is_dir:
+        us = _source.name
+    else:
+        us = _source.stem
+
     # Strip excess periods
-    us = re.sub(r'[!@#$%*—•–’\'\[\]]', "", _source.stem)
+    us = re.sub(r'[!@#$%*—•–’\'\[\]]', "", us)
     us = re.sub(r',', "_", us)
     us = re.sub(r'-_', "-", us)
     us = re.sub(r'_-', "-", us)
     us = re.sub('[-]{2,}', "-", us)
-    _destination = _source.with_stem(us)
+
+    if is_dir:
+        _destination = _source.with_name(us)
+    else:
+        _destination = _source.with_stem(us)
 
     return _destination
 
 
-def delete_excess_periods(source):
+def delete_excess_periods(source, is_dir=None):
     _source = pathlib.Path(source)
     _destination = pathlib.Path(source)
 
+    if is_dir:
+        us = _source.name
+    else:
+        us = _source.stem
     # Strip excess periods
-    us = _source.stem.rstrip(".")
+    us = us.rstrip(".")
     us = re.sub('[.]{2,}', "", us)
-    _destination = _source.with_stem(us)
+
+    if is_dir:
+        _destination = _source.with_name(us)
+    else:
+        _destination = _source.with_stem(us)
 
     return _destination
 
@@ -60,49 +95,62 @@ def main(args):
     cwd = os.getcwd()
     topdown = not args.recursive
 
-    print("Fixing files...")
+    do_files = args.files_only or not args.dirs_only
+    do_dirs = args.dirs_only or not args.files_only
+
+    if do_files:
+        print("Fixing files...")
+    elif do_dirs:
+        print("Gathering dirs...")
+
     _dirs_list = []
     for root, dirs, files in os.walk(cwd, topdown=topdown):
-        print()
-        print(root)
         _dirs_list.append(root)
-        for file in tqdm(files, desc="Files", total=len(files)):
-            source_file = pathlib.Path(root, file)
-            destination_file = ws_to_underscore(source_file)
-            destination_file = delete_excess_periods(destination_file)
-            destination_file = delete_nonsense(destination_file)
-            destination_file = final_strip(destination_file)
 
-            # No Consecutive underscores
-            single_us_file = re.sub("[_]+", "_", destination_file.stem)
-            destination_file = destination_file.with_stem(single_us_file)
-            if args.dry_run and not destination_file.exists():
-                print(destination_file)
-            elif not destination_file.exists():
-                source_file.replace(destination_file)
-            elif source_file != destination_file:
-                print(f'Could not rename due to existing file:\n{source_file}')
+        if do_files:
+            print()
+            print(root)
+            for file in tqdm(files, desc="Files", total=len(files)):
+                source_file = pathlib.Path(root, file)
+                destination_file = ws_to_underscore(source_file, is_dir=False)
+                destination_file = delete_excess_periods(destination_file, is_dir=False)
+                destination_file = delete_nonsense(destination_file, is_dir=False)
+                destination_file = final_strip(destination_file, is_dir=False)
+
+                # No Consecutive underscores
+                single_us_file = re.sub("[_]+", "_", destination_file.stem)
+                destination_file = destination_file.with_stem(single_us_file)
+                if args.dry_run and not destination_file.exists():
+                    print(destination_file)
+                elif not destination_file.exists():
+                    print(destination_file)
+                    source_file.replace(destination_file)
+                elif source_file != destination_file:
+                    print(f'Could not rename due to existing file:\n{source_file}')
 
         if not args.recursive:
             _dirs_list = [str(pathlib.Path(root, x)) for x in dirs]
             break
 
-    print("\nFixing dirs...\n")
-    for dir in tqdm(_dirs_list, desc="Directories", total=len(_dirs_list)):
-        source = pathlib.Path(dir)
-        destination = ws_to_underscore(source)
-        destination = delete_nonsense(destination)
+    if do_dirs:
+        print("\nFixing dirs...\n")
+        for dir in tqdm(_dirs_list, desc="Directories", total=len(_dirs_list)):
+            source = pathlib.Path(dir)
+            destination = ws_to_underscore(source, is_dir=True)
+            destination = delete_excess_periods(destination, is_dir=True)
+            destination = delete_nonsense(destination, is_dir=True)
 
-        # No Consecutive underscores
-        single_us_dir = re.sub("[_]+", "_", destination.stem)
-        destination = destination.with_stem(single_us_dir)
+            # No Consecutive underscores
+            single_us_dir = re.sub("[_]+", "_", destination.name)
+            destination = destination.with_name(single_us_dir)
 
-        if args.dry_run and not destination.exists():
-            print(destination)
-        elif not destination.exists():
-            os.rename(source, destination)
-        elif source != destination:
-            print(f'Could not rename due to existing directory:\n{source}')
+            if args.dry_run and not destination.exists():
+                print(destination)
+            elif not destination.exists():
+                print(destination)
+                os.rename(source, destination)
+            elif source != destination:
+                print(f'Could not rename due to existing directory:\n{source}')
 
 
 # Entrance point
